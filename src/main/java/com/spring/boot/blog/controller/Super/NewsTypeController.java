c8901d81b43bb53fbe3eeeb1418dc3feed443275
ch167 <360132788@qq.com>
2017-11-13 15:21:31 +0800

    2017.11.13 newsType拆分

---
+++ src/main/java/com/spring/boot/blog/controller/Super/NewsTypeController.java
@@ -0,0 +1,111 @@
+package com.spring.boot.blog.controller.Super;
+
+
+import java.util.List;
+
+import javax.validation.ConstraintViolationException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.boot.blog.domain.NewsType;
+import com.spring.boot.blog.domain.ProjectMission;
+import com.spring.boot.blog.domain.User;
+import com.spring.boot.blog.service.NewsTypeService;
+import com.spring.boot.blog.service.ProjectMissionService;
+import com.spring.boot.blog.util.ConstraintViolationExceptionHandler;
+import com.spring.boot.blog.vo.Response;
+
+/**
+ * 后台页面控制器.
+ * 
+ * @date 2017年9月26日
+ */
+@RestController
+@RequestMapping("/super")
+public class NewsTypeController {
+ 
+	@Autowired
+	private NewsTypeService newsTypeService;
+	
+	/**
+	 * 查询所有信息类别
+	 * @return
+	 */
+	@GetMapping("/newsTypeList")
+	public ModelAndView listNewsType(@RequestParam(value="async",required=false) boolean async,
+			@RequestParam(value="pageIndex",required=false,defaultValue="0") int pageIndex,
+			@RequestParam(value="pageSize",required=false,defaultValue="10") int pageSize,
+			@RequestParam(value="messageType",required=false,defaultValue="") String messageType,
+			Model model) {
+	 
+		Pageable pageable = new PageRequest(pageIndex, pageSize);
+		Page<NewsType> page = newsTypeService.listNewsTypesByMessageTypeLike(messageType, pageable);
+		List<NewsType> list = page.getContent();	// 当前所在页面数据列表
+		
+		model.addAttribute("page", page);
+		model.addAttribute("newsTypeList", list);
+		return new ModelAndView(async==true?"newsType/list :: #mainContainerRepleace":"newsType/list", "newsTypeModel", model);
+	}
+	
+	@GetMapping("/addNewsType")
+	public ModelAndView createForm(Model model) {
+		model.addAttribute("newsType", new NewsType(0L,null));
+		return new ModelAndView("newsType/add", "newsTypeModel", model);
+	}
+	
+	
+	@PostMapping("/addNewsType")
+	public ResponseEntity<Response> saveOrUpdate(NewsType newsType) {
+		try {
+			newsTypeService.saveNewsType(newsType);
+		}  catch (ConstraintViolationException e)  {
+			return ResponseEntity.ok().body(new Response(false, ConstraintViolationExceptionHandler.getMessage(e)));
+		}
+		return ResponseEntity.ok().body(new Response(true, "处理成功", newsType));
+	}
+	
+
+	@GetMapping(value = "editNewsType/{id}")
+	public ModelAndView modifyForm(@PathVariable("id") Long id, Model model) {
+		NewsType newsType= newsTypeService.getNewsTypeById(id);
+		model.addAttribute("newsType", newsType);
+		return new ModelAndView("newsType/edit", "newsTypeModel", model);
+	}
+	
+	
+	/**
+	 * 删除信息类别
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping(value = "/newsType/{id}")
+    public ResponseEntity<Response> delete(@PathVariable("id") Long id, Model model) {
+		try {
+			if(newsTypeService.getNewsTypeById(id)!=null){
+			 newsTypeService.removeNewsType(id);
+			}
+		} catch (Exception e) {
+			return  ResponseEntity.ok().body( new Response(false, e.getMessage()));
+		}
+		return  ResponseEntity.ok().body( new Response(true, "处理成功"));
+	}
+	
+
+	
+	
+	
+	 
+}
--- src/main/resources/static/js/super/main.js
+++ src/main/resources/static/js/super/main.js
@@ -11,12 +11,6 @@
 $(function() {
 	
     CKEDITOR.replace( 'mark',{ height: '400px', width: 'auto' } );
-   
-    
-   
-    
-    
-	var _pageSize; // 存储用于搜索
 	
 	// 提交变更后，清空表单
 	$("#update").click(function() {
@@ -53,9 +47,6 @@ $(function() {
 		getMission();
 	});
 	
-	
-	
-	
 	function getMission() {
 		
 		 $.ajax({ 
@@ -75,117 +66,5 @@ $(function() {
 		     }
 		 });
 	}
-	
-	// 根据用户名、页面索引、页面大小获取用户列表
-	function geNewsTypetByMessageType(pageIndex, pageSize) {
-		
-		 $.ajax({ 
-			 url: "/super/newsTypeList", 
-			 contentType : 'application/json',
-			 data:{
-				 "async":true, 
-				 "pageIndex":pageIndex,
-				 "pageSize":pageSize,
-				 "messageType":$("#searchName").val()
-			 },
-			 success: function(data){
-				 $("#mainContainer").html(data);
-		     },
-		     error : function() {
-		    	 toastr.error("error!");
-		     }
-		 });
-	}
-	
-	// 分页
-	$.tbpage("#mainContainer", function (pageIndex, pageSize) {
-		geNewsTypetByMessageType(pageIndex, pageSize);
-		_pageSize = pageSize;
-	});
-   
-	// 搜索
-	$("#searchNameBtn").click(function() {
-	
-		geNewsTypetByMessageType(0, _pageSize);
-	});
-	
-	
-	// 获取添加用户的界面
-	$("#addNewsType").click(function() {
-		$.ajax({ 
-			 url: "/super/addNewsType", 
-			 success: function(data){
-				
-				 $("#formContainer").html(data);
-		     },
-		     error : function(data) {
-		    	 toastr.error("error!");
-		     }
-		 });
-	});
-	
-	// 获取编辑信息的界面
-	$("#rightContainer").on("click",".super-edit-newsType", function () { 
-		$.ajax({ 
-			 url: "/super/editNewsType/" + $(this).attr("newsTypeId"), 
-			 success: function(data){
-				 $("#formContainer").html(data);
-		     },
-		     error : function() {
-		    	 toastr.error("error!");
-		     }
-		 });
-		
-	});
-	
-	
-	
-	// 提交变更后，清空表单
-	$("#submitEdit").click(function() {
-		$.ajax({ 
-			 url: "/super/addNewsType", 
-			 type: 'POST',
-			 data:$('#newsTypeForm').serialize(),
-			 success: function(data){
-				 $('#newsTypeForm')[0].reset();  
-				 
-				 if (data.success) {
-					 // 从新刷新主界面
-					 geNewsTypetByMessageType(0, _pageSize);
-				 } else {
-					 toastr.error(data.message);
-				 }
-
-		     },
-		     error : function() {
-		    	 toastr.error("error!");
-		     }
-		 });
-	});
-	
-	// 删除信息内容
-	$("#rightContainer").on("click",".super-delete-newsType", function () { 
-		// 获取 CSRF Token 
-		var csrfToken = $("meta[name='_csrf']").attr("content");
-		var csrfHeader = $("meta[name='_csrf_header']").attr("content");
-		$.ajax({ 
-			 url: "/super/newsType/" + $(this).attr("newsTypeId") , 
-			 type: 'DELETE', 
-			 beforeSend: function(request) {
-                 request.setRequestHeader(csrfHeader, csrfToken); // 添加  CSRF Token 
-             },
-			 success: function(data){
-				 if (data.success) {
-					 // 从新刷新主界面
-					 geNewsTypetByMessageType(0, _pageSize);
-				 } else {
-					 toastr.error(data.message);
-				 }
-		     },
-		     error : function() {
-		    	 toastr.error("error!");
-		     }
-		 });
-	});
 	
 });
