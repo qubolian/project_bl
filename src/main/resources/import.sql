3491d50018c06df8da24577a73f01674bbb16ef1
maigc <qubolian@126.com>
2017-11-15 11:42:22 +0800

    个人资料修改 url

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -14,7 +14,7 @@ INSERT INTO user_authority (user_id, authority_id) VALUES (1, 3);
 INSERT INTO user_authority (user_id, authority_id) VALUES (1, 5);
 
 
-INSERT INTO project_mission (id, mark,misson) VALUES (1, '这是个宗旨的内容','测试一下');
+INSERT INTO project_mission (id, mark,misson) VALUES (1, '请输入宗旨的内容','测试一下');
 
 insert into Department_List(Code,Department) values(080901,'计算器科学与技术系');
 insert into Department_List(Code,Department) values(080902,'软件工程系');
