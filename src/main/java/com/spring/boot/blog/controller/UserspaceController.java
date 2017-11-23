4e36091ced504255d425d4b26e90aa7dc946d89f
maigc <qubolian@126.com>
2017-11-23 11:18:41 +0800

    修改个人设置

--- src/main/java/com/spring/boot/blog/controller/UserspaceController.java
+++ src/main/java/com/spring/boot/blog/controller/UserspaceController.java
@@ -2,10 +2,7 @@ package com.spring.boot.blog.controller;
 
 import java.util.List;
 
-import javax.validation.ConstraintViolationException;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -16,8 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -33,131 +28,127 @@ import com.spring.boot.blog.domain.Blog;
 import com.spring.boot.blog.domain.User;
 import com.spring.boot.blog.service.BlogService;
 import com.spring.boot.blog.service.UserService;
-import com.spring.boot.blog.util.ConstraintViolationExceptionHandler;
 import com.spring.boot.blog.vo.Response;
 
 /**
  * 用户主页空间控制器.
  * 
  * @since 1.0.0 2017年3月25日
- * @author <a href="https://waylau.com">Way Lau</a> 
+ * @author <a href="https://waylau.com">Way Lau</a>
  */
 @Controller
 @RequestMapping("/u")
 public class UserspaceController {
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
-	private BlogService blogService; 
-	
+	private BlogService blogService;
+
 	@Value("${file.server.url}")
 	private String fileServerUrl;
-	
+
 	@GetMapping("/{username}")
 	public String userSpace(@PathVariable("username") String username, Model model) {
-		User  user = (User)userDetailsService.loadUserByUsername(username);
+		User user = (User) userDetailsService.loadUserByUsername(username);
 		model.addAttribute("user", user);
 		return "redirect:/u/" + username + "/blogs";
 	}
- 
 
 	@GetMapping("/{username}/profile")
-	@PreAuthorize("authentication.name.equals(#username)") 
+	// @PreAuthorize("authentication.name.equals(#username)")
 	public ModelAndView profile(@PathVariable("username") String username, Model model) {
-		User  user = (User)userDetailsService.loadUserByUsername(username);
+		User user = (User) userDetailsService.loadUserByUsername(username);
 		model.addAttribute("user", user);
-		model.addAttribute("fileServerUrl",fileServerUrl);
+		model.addAttribute("fileServerUrl", fileServerUrl);
 		return new ModelAndView("/userspace/profile", "userModel", model);
 	}
- 
-	
-	
+
 	@PostMapping("/{username}/profile")
-	@PreAuthorize("authentication.name.equals(#username)") 
-	public String saveProfile( User user, @PathVariable("username") String username,@RequestParam(value="newPassword",required=false) String newPassword) {
-		
-			User originalUser = userService.getUserById(user.getId());
-			originalUser.setEmail(user.getEmail());
-			originalUser.setName(user.getName());
-			
-			/*// 判断密码是否做了变更
-			String rawPassword = originalUser.getPassword();
-			PasswordEncoder  encoder = new BCryptPasswordEncoder();
-			String encodePasswd = encoder.encode(user.getPassword());
-			boolean isMatch = encoder.matches(rawPassword, encodePasswd);
-			if (!isMatch) {
-				originalUser.setEncodePassword(user.getPassword());
-			}*/
-			// 判断密码是否做了变更
-			if(newPassword != "") {
-				originalUser.setEncodePassword(newPassword);
-			}
-			
-			userService.saveUser(originalUser);
-			return "redirect:/u/" + username + "/profile";
-	
+	// @PreAuthorize("authentication.name.equals(#username)")
+	public String saveProfile(User user, @PathVariable("username") String username,
+			@RequestParam(value = "newPassword", required = false) String newPassword) {
+
+		User originalUser = userService.getUserById(user.getId());
+		originalUser.setEmail(user.getEmail());
+		originalUser.setName(user.getName());
+
+		/*
+		 * // 判断密码是否做了变更 String rawPassword = originalUser.getPassword();
+		 * PasswordEncoder encoder = new BCryptPasswordEncoder(); String
+		 * encodePasswd = encoder.encode(user.getPassword()); boolean isMatch =
+		 * encoder.matches(rawPassword, encodePasswd); if (!isMatch) {
+		 * originalUser.setEncodePassword(user.getPassword()); }
+		 */
+		// 判断密码是否做了变更
+		if (newPassword != "") {
+			originalUser.setEncodePassword(newPassword);
+		}
+
+		userService.saveUser(originalUser);
+		return "redirect:/u/" + username + "/profile";
+
 	}
-	
+
 	/**
 	 * 保存头像
+	 * 
 	 * @param username
 	 * @param model
 	 * @return
 	 */
 	@PostMapping("/{username}/avatar")
-	@PreAuthorize("authentication.name.equals(#username)") 
+	@PreAuthorize("authentication.name.equals(#username)")
 	public ResponseEntity<Response> saveAvatar(@PathVariable("username") String username, @RequestBody User user) {
 		String avatarUrl = user.getAvatar();
-		
+
 		User originalUser = userService.getUserById(user.getId());
 		originalUser.setAvatar(avatarUrl);
 		userService.saveUser(originalUser);
-		
+
 		return ResponseEntity.ok().body(new Response(true, "处理成功", avatarUrl));
 	}
+
 	/**
 	 * 获取编辑头像的界面
+	 * 
 	 * @param username
 	 * @param model
 	 * @return
 	 */
 	@GetMapping("/{username}/avatar")
-	@PreAuthorize("authentication.name.equals(#username)") 
+	@PreAuthorize("authentication.name.equals(#username)")
 	public ModelAndView avatar(@PathVariable("username") String username, Model model) {
-		User  user = (User)userDetailsService.loadUserByUsername(username);
+		User user = (User) userDetailsService.loadUserByUsername(username);
 		model.addAttribute("user", user);
 		return new ModelAndView("/userspace/avatar", "userModel", model);
 	}
-	
-	
+
 	@GetMapping("/{username}/blogs")
 	public String listBlogsByOrder(@PathVariable("username") String username,
-			@RequestParam(value="order",required=false,defaultValue="new") String order,
-			@RequestParam(value="category",required=false ) Long category,
-			@RequestParam(value="keyword",required=false,defaultValue="" ) String keyword,
-			@RequestParam(value="async",required=false) boolean async,
-			@RequestParam(value="pageIndex",required=false,defaultValue="0") int pageIndex,
-			@RequestParam(value="pageSize",required=false,defaultValue="10") int pageSize,
-			Model model) {
-		User  user = (User)userDetailsService.loadUserByUsername(username);
+			@RequestParam(value = "order", required = false, defaultValue = "new") String order,
+			@RequestParam(value = "category", required = false) Long category,
+			@RequestParam(value = "keyword", required = false, defaultValue = "") String keyword,
+			@RequestParam(value = "async", required = false) boolean async,
+			@RequestParam(value = "pageIndex", required = false, defaultValue = "0") int pageIndex,
+			@RequestParam(value = "pageSize", required = false, defaultValue = "10") int pageSize, Model model) {
+		User user = (User) userDetailsService.loadUserByUsername(username);
 		model.addAttribute("user", user);
-		
+
 		if (category != null) {
-			
-			System.out.print("category:" +category );
-			System.out.print("selflink:" + "redirect:/u/"+ username +"/blogs?category="+category);
+
+			System.out.print("category:" + category);
+			System.out.print("selflink:" + "redirect:/u/" + username + "/blogs?category=" + category);
 			return "/u";
-			
-		}   
-		
-		
+
+		}
+
 		Page<Blog> page = null;
 		if (order.equals("hot")) { // 最热查询
-			Sort sort = new Sort(Direction.DESC,"reading","comments","likes"); 
+			Sort sort = new Sort(Direction.DESC, "reading", "comments", "likes");
 			Pageable pageable = new PageRequest(pageIndex, pageSize, sort);
 			page = blogService.listBlogsByTitleLikeAndSort(user, keyword, pageable);
 		}
@@ -165,47 +156,48 @@ public class UserspaceController {
 			Pageable pageable = new PageRequest(pageIndex, pageSize);
 			page = blogService.listBlogsByTitleLike(user, keyword, pageable);
 		}
- 
-		
-		List<Blog> list = page.getContent();	// 当前所在页面数据列表
-		
+
+		List<Blog> list = page.getContent(); // 当前所在页面数据列表
+
 		model.addAttribute("order", order);
 		model.addAttribute("page", page);
 		model.addAttribute("blogList", list);
-		return (async==true?"/userspace/u :: #mainContainerRepleace":"/userspace/u");
+		return (async == true ? "/userspace/u :: #mainContainerRepleace" : "/userspace/u");
 	}
-	
+
 	/**
 	 * 获取博客展示界面
+	 * 
 	 * @param id
 	 * @param model
 	 * @return
 	 */
 	@GetMapping("/{username}/blogs/{id}")
-	public String getBlogById(@PathVariable("username") String username,@PathVariable("id") Long id, Model model) {
+	public String getBlogById(@PathVariable("username") String username, @PathVariable("id") Long id, Model model) {
 		// 每次读取，简单的可以认为阅读量增加1次
 		blogService.readingIncrease(id);
-		
+
 		boolean isBlogOwner = false;
-		
+
 		// 判断操作用户是否是博客的所有者
-		if (SecurityContextHolder.getContext().getAuthentication() !=null && SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
-				 &&  !SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString().equals("anonymousUser")) {
-			User principal = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal(); 
-			if (principal !=null && username.equals(principal.getUsername())) {
+		if (SecurityContextHolder.getContext().getAuthentication() != null
+				&& SecurityContextHolder.getContext().getAuthentication().isAuthenticated() && !SecurityContextHolder
+						.getContext().getAuthentication().getPrincipal().toString().equals("anonymousUser")) {
+			User principal = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			if (principal != null && username.equals(principal.getUsername())) {
 				isBlogOwner = true;
-			} 
+			}
 		}
- 
+
 		model.addAttribute("isBlogOwner", isBlogOwner);
-		model.addAttribute("blogModel",blogService.getBlogById(id));
-		
+		model.addAttribute("blogModel", blogService.getBlogById(id));
+
 		return "/userspace/blog";
 	}
-	
-	
+
 	/**
 	 * 获取新增博客的界面
+	 * 
 	 * @param model
 	 * @return
 	 */
@@ -214,61 +206,63 @@ public class UserspaceController {
 		model.addAttribute("blog", new Blog(null, null, null));
 		return new ModelAndView("/userspace/blogedit", "blogModel", model);
 	}
-	
+
 	/**
 	 * 获取编辑博客的界面
+	 * 
 	 * @param model
 	 * @return
 	 */
 	@GetMapping("/{username}/blogs/edit/{id}")
-	public ModelAndView editBlog(@PathVariable("username") String username,@PathVariable("id") Long id, Model model) {
+	public ModelAndView editBlog(@PathVariable("username") String username, @PathVariable("id") Long id, Model model) {
 		model.addAttribute("blog", blogService.getBlogById(id));
 		return new ModelAndView("/userspace/blogedit", "blogModel", model);
 	}
-	
+
 	/**
 	 * 保存博客
+	 * 
 	 * @param username
 	 * @param blog
 	 * @return
 	 */
 	@PostMapping("/{username}/blogs/edit")
-	@PreAuthorize("authentication.name.equals(#username)") 
+	@PreAuthorize("authentication.name.equals(#username)")
 	public ResponseEntity<Response> saveBlog(@PathVariable("username") String username, @RequestBody Blog blog) {
-		User user = (User)userDetailsService.loadUserByUsername(username);
+		User user = (User) userDetailsService.loadUserByUsername(username);
 		blog.setUser(user);
 		try {
 			blogService.saveBlog(blog);
-		}catch (RuntimeException e)  {
+		} catch (RuntimeException e) {
 			Throwable cause = e.getCause();
-		    if(cause instanceof javax.persistence.RollbackException) {
-		    	return ResponseEntity.ok().body(new Response(false, "更改值有误，请重试！"));
-		    }
-		}catch (Exception e) {
+			if (cause instanceof javax.persistence.RollbackException) {
+				return ResponseEntity.ok().body(new Response(false, "更改值有误，请重试！"));
+			}
+		} catch (Exception e) {
 			return ResponseEntity.ok().body(new Response(false, e.getMessage()));
 		}
-		
+
 		String redirectUrl = "/u/" + username + "/blogs/" + blog.getId();
 		return ResponseEntity.ok().body(new Response(true, "处理成功", redirectUrl));
 	}
-	
-	
+
 	/**
 	 * 删除博客
+	 * 
 	 * @param id
 	 * @param model
 	 * @return
 	 */
 	@DeleteMapping("/{username}/blogs/{id}")
-	@PreAuthorize("authentication.name.equals(#username)") 
-	public ResponseEntity<Response> deleteBlog(@PathVariable("username") String username,@PathVariable("id") Long id) {
-		
+	@PreAuthorize("authentication.name.equals(#username)")
+	public ResponseEntity<Response> deleteBlog(@PathVariable("username") String username, @PathVariable("id") Long id) {
+
 		try {
 			blogService.removeBlog(id);
 		} catch (Exception e) {
 			return ResponseEntity.ok().body(new Response(false, e.getMessage()));
 		}
-		
+
 		String redirectUrl = "/u/" + username + "/blogs";
 		return ResponseEntity.ok().body(new Response(true, "处理成功", redirectUrl));
 	}
