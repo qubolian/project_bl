cafdeac93aa015adbf84aa81cfc712fcbbbd458d
ch167 <360132788@qq.com>
2017-11-27 17:41:42 +0800

    2017.11.27 指导老师完善个人信息完成

--- src/main/java/com/spring/boot/blog/controller/supervisor/PerfectInformationController.java
+++ src/main/java/com/spring/boot/blog/controller/supervisor/PerfectInformationController.java
@@ -1,6 +1,14 @@
 package com.spring.boot.blog.controller.supervisor;
 
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.ConstraintViolationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +16,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.spring.boot.blog.domain.SubmitFile;
 import com.spring.boot.blog.domain.Teacher;
 import com.spring.boot.blog.domain.Teacher;
 import com.spring.boot.blog.domain.User;
 import com.spring.boot.blog.service.TeacherService;
 import com.spring.boot.blog.service.TeacherService;
 import com.spring.boot.blog.util.ConstraintViolationExceptionHandler;
+import com.spring.boot.blog.util.FileUtil;
 import com.spring.boot.blog.vo.Response;
 
 /**
@@ -33,6 +47,8 @@ public class PerfectInformationController {
 	@Autowired
 	private TeacherService teacherService;
 	
+	private static final String localURL = "D:/image/";
+	
 	/**
 	 * 获取后台管理主页面
 	 * @return
@@ -46,6 +62,10 @@ public class PerfectInformationController {
 		return new ModelAndView("perfectInformation/list", "model", model);
 	}
 	
+	/**
+	 * 获取教师信息
+	 * @return
+	 */
 	@GetMapping("/teacherInformation")
 	public ResponseEntity<Response> TeacherShow() {
 		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -59,6 +79,12 @@ public class PerfectInformationController {
 		return ResponseEntity.ok().body(new Response(true, "显示成功", teacher));
 	}
 	
+	/**
+	 * 更改教师信息
+	 * @param expert
+	 * @param expectStudent
+	 * @return
+	 */
 	@GetMapping("/updateTeacherInformation")
 	public ResponseEntity<Response> TeacherEdit(String expert,String expectStudent){
 		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -74,6 +100,110 @@ public class PerfectInformationController {
 			return ResponseEntity.ok().body(new Response(false, e.getMessage()));
 		}
 		return ResponseEntity.ok().body(new Response(true, "更新成功", teacher));
+	}
+	
+	/**
+	 * 上传教学进度表
+	 * 
+	 * @param file
+	 * @return
+	 */
+	@PostMapping("/uploadResume")
+	public ResponseEntity<Response> saveSchedule(
+			@RequestParam("resume") MultipartFile resume) {
+		
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Long id = Long.valueOf(user.getUsername());
+		
+		String fileName = resume.getOriginalFilename();
+		String filePath = localURL;
+		SimpleDateFormat tempDate = new SimpleDateFormat("YYYYMMddHHmmss");
+		String datetime = tempDate.format(new Date(System.currentTimeMillis()));
+		
+		try {
+			FileUtil.uploadFile(resume.getBytes(), filePath,
+					datetime + id + "resume" + fileName.substring(fileName.lastIndexOf(".")));
+			
+			Teacher teacher = teacherService.getTeacherById(id);
+			teacher.setResume(fileName);
+			teacher.setResumeSaveName(datetime + id + "resume" + fileName.substring(fileName.lastIndexOf(".")));
+			teacherService.saveTeacher(teacher);
+			
+		} catch (Exception e) {
+			return ResponseEntity.ok().body(new Response(false, e.getMessage()));
+		}
+		return ResponseEntity.ok().body(new Response(true, "处理成功", fileName));
+
+	}
+	
+	/**
+	 * 删除教学进度表
+	 * 
+	 * @param file
+	 * @return
+	 */
+	@GetMapping("/deleteResume")
+	public ResponseEntity<Response> deleteSchedule() {
+		try {
+			
+			User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			Long id = Long.valueOf(user.getUsername());
+
+			Teacher teacher = teacherService.getTeacherById(id);
+			
+			File folder = new File(localURL);
+			File[] files = folder.listFiles();
+			for (File file : files) {
+				if (file.getName().equals(teacher.getResume())) {
+					file.delete();
+				}
+			}
+			
+			teacher.setResume("0");
+			teacher.setResumeSaveName("0");
+			teacherService.saveTeacher(teacher);
+
+		} catch (Exception e) {
+			return ResponseEntity.ok().body(new Response(false, e.getMessage()));
+		}
+		return ResponseEntity.ok().body(new Response(true, "处理成功"));
+
+	}
+	
+	/**
+	 * 下载教学进度表
+	 * 
+	 * @param file
+	 * @return
+	 */
+	@GetMapping("/downloadResume")
+	public void downloadSchedule(HttpServletResponse response){
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		Long id = Long.valueOf(user.getUsername());
+		
+		Teacher teacher = teacherService.getTeacherById(id);
+			
+		String realPath = localURL+teacher.getResumeSaveName();//获取要下载的文件的绝对路径
+		
+		String fileName = teacher.getResume();//获取要下载的文件名
+		try{
+			//设置content-disposition响应头控制浏览器以下载的形式打开文件，中文文件名要使用URLEncoder.encode方法进行编码，否则会出现文件名乱码
+			response.setHeader("Content-Disposition", "attachment; filename=\"" + new String(fileName.getBytes("gbk"),"iso-8859-1") + "\""); 
+	        response.setContentType("application/octet-stream;charset=UTF-8");
+			InputStream in;
+			//获取文件输入流	
+			in = new FileInputStream(realPath);
+			int len = 0;
+			byte[] buffer = new byte[1024];
+			OutputStream out = response.getOutputStream();
+			while ((len = in.read(buffer)) > 0) {
+				out.write(buffer,0,len);//将缓冲区的数据输出到客户端浏览器
+			}
+			in.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 	
 	
--- src/main/resources/static/js/supervisor/supervisor.js
+++ src/main/resources/static/js/supervisor/supervisor.js
@@ -6,12 +6,30 @@
  */
 "use strict";
 //# sourceURL=main.js
- 
+$('#resumeName').attr('href','/supervisor/downloadResume/');
 // DOM 加载完再执行
 $(function() {
 	
     CKEDITOR.replace( 'expert',{ height: '400px', width: 'auto' } );
     CKEDITOR.replace( 'expectStudent',{ height: '400px', width: 'auto' } );
+	
+	function getTeacherInformation() {
+		 $.ajax({ 
+			 url: "/supervisor/teacherInformation", 
+			 contentType : 'application/json',
+			 success: function(data){
+				 console.log(data);
+				 CKEDITOR.instances.expert.setData(data.body.expert);
+				 CKEDITOR.instances.expectStudent.setData(data.body.expectStudent);
+				 $("#expert").val(data.body.expert);
+				 $("#expectStudent").val(data.body.expectStudent);
+		     },
+		     error : function() {
+		    	 toastr.error("error!");
+		     }
+		 });
+	}
+	
     getTeacherInformation();
 
 	
@@ -49,25 +67,66 @@ $(function() {
 		getTeacherInformation();
 	});
 	
-	function getTeacherInformation() {
-		 $.ajax({ 
-			 url: "/supervisor/teacherInformation", 
-			 contentType : 'application/json',
+	
+	// 上传履历表
+	$("#uploadResume").on("click",function(){
+		var div = $('#1');
+		var del = $(".supervisor-delete-resume");
+		var id = $("#id1").val();
+		if($("#resumelabel").length > 0){
+			toastr.error("请先删除现有履历表！");
+		}else if($("#resume").val() == ""){
+			toastr.error("请先选择履历表！");
+		}else{
+			var formData = new FormData($( "#resumeForm" )[0]);
+			$.ajax({ 
+				 url: "/supervisor/uploadResume", 
+				 type: 'POST',
+				 data:formData,
+				 cache: false,  
+		         processData: false,  
+		         contentType: false,
+				 success: function(data){
+					 $('#resumeForm')[0].reset(); 
+					 if (data.success) {
+						 div.prepend(
+									 "<label id='resumelabel' class='col-form-label'><strong style='color:red'>*&thinsp;</strong><a href='/supervisor/downloadResume/' id='resumeName'>"+data.body+"</a></label>" 
+						 );
+						 del.show();
+						 
+					 } else {
+						 toastr.error(data.message);
+					 }
+	
+			     },
+			     error : function() {
+			    	 toastr.error("error!");
+			     }
+			 });
+		}
+	});	
+	
+	// 删除教学进度表
+	$(".supervisor-delete-resume").on("click",function(){
+		$.ajax({ 
+			 url: "/supervisor/deleteResume", 
 			 success: function(data){
-				 console.log(data);
-				 CKEDITOR.instances.expert.setData(data.body.expert);
-				 CKEDITOR.instances.expectStudent.setData(data.body.expectStudent);
-				 $("#expert").val(data.body.expert);
-				 $("#expectStudent").val(data.body.expectStudent);
+				 
+				 if (data.success) {
+					 // 从新刷新主界面
+					 $("#resumelabel").remove();
+					 $(".supervisor-delete-resume").hide();
+					 
+				 } else {
+					 toastr.error(data.message);
+				 }
+
 		     },
 		     error : function() {
 		    	 toastr.error("error!");
 		     }
 		 });
-	}
-	
-	
-	
+	});
 	
 	
 });
